f28d627bf1b71e16c1523edba2451d0b94595b41
godwish1234 <godwish_jakin@hotmail.com>
2018-04-11 09:07:55 +1000

    9th Commit
    Read data and write data to the device

--- SIT305/GameViewController.swift
+++ SIT305/GameViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import Foundation
 
 class GameViewController: UIViewController {
 
+    let jsonString = "[" +
+        "{" +
+    "   \"id\":\"8\"" +
+    "}" +
+    " ]"
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +30,123 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func save(_ sender: UIButton) {
+        
+        /*
+        let dataFromString: Data? = jsonString.data(using: String.Encoding.utf8)
+        
+        guard let data = dataFromString else {
+            print("error")
+            return
+        }
+        
+        do {
+            let parsedData = try JSONSerialization.jsonObject(with: data, options: []) as! [[String:Any]]
+        } catch let error {
+            print(error)
+        }
+        */
+        
+        /*
+        let fileName = "item"
+        
+        let file: FileHandle? = FileHandle(forWritingAtPath: "items.json")
+        if file != nil {
+            do {
+                if let jsonData = try JSONSerialization.data(withJSONObject: fileName, options: .init(rawValue: 0)) as? Data
+                {
+                    print(NSString(data: jsonData, encoding: 1)!)
+                    file?.write(jsonData)
+                }
+            }
+            catch {
+                
+            }
+            file?.closeFile()
+        }
+        else{
+            print("something went wrong")
+        }
+ */
+        
+        /*
+        let file = "items"
+        
+        
+        
+        let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        
+        
+        if let fileURL = dir?.appendingPathComponent(file).appendingPathExtension("json"){
+        
+            
+            
+            var inString = ""
+            do {
+                inString = try String(contentsOf: fileURL)
+            } catch {
+                print("Failed reading from URL: \(fileURL), Error: " + error.localizedDescription)
+            }
+            print("Read from the file: \(inString)")
+            
+        
+        }*/
+        
+        var test: [AnyObject] = []
+        var dict: [String: AnyObject] = [:]
+        dict["id"] = "8" as AnyObject
+        test.append(dict as AnyObject)
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: test, options: .prettyPrinted)
+            let fileManager = FileManager.default
+            let url = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let jsonURL = url.appendingPathComponent("items.json")
+            //print(jsonURL)
+            
+            //try jsonData.write(to: jsonURL)
+            
+            
+            
+        }
+        catch{
+            
+        }
+        
+        /*
+        do {
+            
+            let fileManager = FileManager.default
+            let url = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let jsonURL = url.appendingPathComponent("items.json")
+            let data = try Data(contentsOf: url)
+            let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+            print(json)
+            
+        }
+        catch {
+            print(error)
+        }*/
+        
+        var inString = ""
+        
+        do{
+            let fileManager = FileManager.default
+            let url = try fileManager.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+            let jsonURL = url.appendingPathComponent("items.json")
+            inString = try String (contentsOf: jsonURL)
+        }
+        catch
+        {
+            print("Erro reading" + error.localizedDescription)
+        }
+        print("Read From the file: \(inString)")
+        
+        
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
