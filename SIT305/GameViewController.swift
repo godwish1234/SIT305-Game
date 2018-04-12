c491ad515f403a46a522607d30f7ad8182444ed5
nichoolashan <nicholas.han97@yahoo.com>
2018-04-12 15:34:32 +1000

    nicho

--- SIT305/GameViewController.swift
+++ SIT305/GameViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class GameViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,7 +23,50 @@ class GameViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func Booster(_ sender: UIButton) {
+        
+    
+        let jalur = "items"
+        
+        
+        
+        let dir = try? FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+       
+        if let fileURL = dir?.appendingPathComponent(jalur).appendingPathExtension("json"){
+            
+            let value = "Handphone"
+            
+            do{
+                
+            try value.write(to: fileURL, atomically: false, encoding: .utf8)
+                
+            print(fileURL)
+            
+        }
+        catch let error as NSError{
+            print("Failed to store into the URL")
+            print (error)
+            }
+            
+            var inString = ""
+            
+            do{
+                inString = try String (contentsOf: fileURL)
+            }
+            catch
+            {
+                print("Erro reading" + error.localizedDescription)
+            }
+            print("Read From the file: \(inString)")
+    }
+        
+        //let dict = NSMutableDictionary()
+        //dict.setValue(value, forKey: "name")
+        //dict.write(to: url, atomically: true)
+        
+        
+    }
+    
     /*
     // MARK: - Navigation
 
